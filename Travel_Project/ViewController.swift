0b0781e91da08afcbe1586a15744425f6298e3ef
Christian <s175125@student.dtu.dk>
2019-11-11 11:18:59 -0600

    Initial Commit

---
+++ Travel_Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Travel_Project
+//
+//  Created by Hoej, Christian R on 11/11/19.
+//  Copyright © 2019 Hoej, Christian R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
