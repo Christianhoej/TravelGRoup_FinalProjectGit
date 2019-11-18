a522bd2b35e0e7e4f72339683083d90e8324d861
christianhoej <s175125@student.dtu.dk>
2019-11-18 17:09:02 -0600

    part1

---
+++ Travel_Project/Countrydata.swift
@@ -0,0 +1,119 @@
+//
+//  Countrydata.swift
+//  Travel_Project
+//
+//  Created by Hoej, Christian R on 11/18/19.
+//  Copyright © 2019 Hoej, Christian R. All rights reserved.
+//
+
+import UIKit
+
+protocol CountryDataProtocol {
+    func responseDataHandler(data: NSDictionary)
+    func responseError(message:String)
+    
+}
+
+struct Data: Decodable{
+    let population: String
+    let capital: String
+    let languages: String
+    let religion: String
+    let government: String
+    let area: String
+    let currency: String
+    let gdp: String
+}
+
+class Countrydata: NSObject {
+    
+    private let urlSession = URLSession.shared
+    private let urlPathBase = "https://restcountries.eu/rest/v2/name/"
+        
+        
+        private var dataTask:URLSessionDataTask? = nil
+    
+    override init() {}
+        
+        var delegate:CountryDataProtocol? = nil
+        
+       
+        func getCountry(name: String) {
+            
+            var urlPath = self.urlPathBase
+            var countryName = name.replacingOccurrences(of: " ", with: "+")
+            var temp = "\(countryName)?fullText=true"
+            
+            urlPath = urlPath + temp
+            
+            let url:NSURL? = NSURL(string: urlPath)
+            
+            //let url = NSURL(string: "https://restcountries.eu/rest/v2/name/\(name)?fullText=true")
+            
+            let dataTask = self.urlSession.dataTask(with: url! as URL) { (data, response, error) -> Void in
+                if error != nil {
+                    print(error!)
+                } else {
+                    do {
+                        let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary
+                        
+                        if let dictionary = data as? [String: Any] {
+                            print("1")
+                            if let number = dictionary["name"] as? String {
+                                // access individual value in dictionary
+                                print("WHAT")
+                            }
+
+                            for (key, value) in dictionary {
+                                // access all key / value pairs in dictionary
+                            }
+
+                            if let nestedDictionary = dictionary["anotherKey"] as? [String: Any] {
+                                // access nested dictionary values by key
+                            }
+                        }
+                        
+                        
+                        
+              
+                        /*if jsonResult != nil {
+                            print("jsonResult")
+                            let name: String? = jsonResult!["name"] as? String
+                            let capital: String? = jsonResult!["capital"] as? String
+                            if name != nil && capital != nil {
+                                self.delegate?.responseDataHandler(data: jsonResult!)
+                                print("WWWWWWWWWWWW")
+                            } else {
+                                print("FEEEEEEJL")
+                                self.delegate?.responseError(message: "Fake data not found")
+                            }
+                        }
+                        else{
+                            print("FFFFFFF")
+                        }*/
+                        
+                        /*guard let data1 = jsonResult as? [NSDictionary] else {
+                              return
+                        }
+                        guard let current = data1 as? [NSDictionary] else {
+                              return
+                        }*/
+                        
+                        print("Raw data:\n\(data!)\n")
+                        let dataString = String(data: data!, encoding: String.Encoding.utf8)
+                        print("Human-readable data:\n\(dataString!)")
+                    } catch {
+                    }
+                }
+            }
+            dataTask.resume()
+        }
+    }
+
+    
+    
+
+
+
+
+
--- Travel_Project/ViewController.swift
+++ Travel_Project/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var selectCountry: UIButton!
+    @IBOutlet weak var testLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -19,7 +22,34 @@ class ViewController: UIViewController {
         //kalgpiusgh;;
         //rewrytwertrwetrew
     }
+    
+    let datasession = Countrydata();
+    
+    @IBAction func selectCountry(_ sender: Any) {
+        datasession.getCountry(name: "denmark")
+    }
+    
+    //MARK: Example Data Protocol
+    
+    func responseDataHandler(data:NSDictionary) {
+        //let title = data["title"] as! String
+        let name = data["name"] as! String
+        
+        //Run this handling on a separate thread
+        DispatchQueue.main.async() {
+            self.testLabel.text = name
+            //self.bodyTextView.text = body
+        }
+    }
 
+    func responseError(message:String) {
+        //Run this handling on a separate thread
+        DispatchQueue.main.async() {
+            self.testLabel.text = "Error!"
+            //self.bodyTextView.text = message
+        }
+    }
+    
 
 }
 
